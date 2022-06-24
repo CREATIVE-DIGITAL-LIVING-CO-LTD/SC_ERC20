c2a18c51631c0553da3ff45a74a26be648fc8da2
pasuttbp <pasut.t@creativeme.co.th>
2022-06-24 10:59:50 +0700

    COMMIT

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,19 +2,12 @@ import { ethers } from 'hardhat'
 
 async function main() {
     const ANIV20 = await ethers.getContractFactory('ANIV20')
-    const {
-        ADDRESS_OWNER,
-        ADDRESS_2022,
-        ADDRESS_2023,
-        ADDRESS_2024,
-        ADDRESS_2025,
-    } = process.env
     const contract = await ANIV20.deploy(
-        ADDRESS_OWNER as string,
-        ADDRESS_2023 as string,
-        ADDRESS_2022 as string,
-        ADDRESS_2024 as string,
-        ADDRESS_2025 as string
+        process.env.ADDRESS_OWNER as string,
+        process.env.ADDRESS_2022 as string,
+        process.env.ADDRESS_2023 as string,
+        process.env.ADDRESS_2024 as string,
+        process.env.ADDRESS_2025 as string
     )
     await contract.deployed()
     console.log('Token address:', contract.address)
